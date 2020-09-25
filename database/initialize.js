f276fc89f778ba2b66e4f82264ae528dae5f2567
Stuzerain <shanford3@gmail.com>
2020-09-25 14:27:05 -0700

    Finish seeding database with photos table

--- database/initialize.js
+++ database/initialize.js
@@ -2,6 +2,7 @@ const Promise = require('bluebird');
 const faker = require('faker');
 
 var seededProducts = [];
+var seededPhotos = []
 
 module.exports = (db) => {
   if (db.queryAsync) {
@@ -53,6 +54,100 @@ module.exports = (db) => {
     .then(() => db.queryAsync(`
     INSERT IGNORE INTO Products (product) VALUES ?`,
       [seededProducts]))
+    .then(() => console.log('finished seeding Products table, moving on to Photos table'))
+
+
+    // Insert deliberate Photos for testing -- these will relate
+    // to specific products to show that the db tables connect properly
+    // and the API correctly fetches the data for a product from both
+
+    // the first set of photos correlates to Product 1 in the Products table,
+    // the Pumpkin Pie costume. Since this will be the default product, it will have
+    // the most photos to show off the image carousel at its best
+    .then(() => {
+      var photos = [
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie1.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie2.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie3.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie4.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie5.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie6.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie7.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie8.jpg', 1],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pumpkinpie9.jpg', 1]]
+      return db.queryAsync(`
+      INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+        [photos])
+    })
+
+    .then(() => {
+      var photos = [
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast1.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast2.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast3.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast4.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast5.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast6.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast7.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast8.jpg', 2],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/breakfast9.jpg', 2]]
+      return db.queryAsync(`
+      INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+        [photos])
+    })
+
+    .then(() => {
+      var photos = [
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza1.jpg', 3],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza2.jpg', 3],
+        ['https://petsy-caro3usel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza3.jpg', 3],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza4.jpg', 3],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza5.jpg', 3],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/pizza6.jpg', 3]]
+      return db.queryAsync(`
+      INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+        [photos])
+    })
+
+    .then(() => {
+      var photos = [
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark1.jpg', 4],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark2.jpg', 4],
+        ['https://petsy-caro3usel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark3.jpg', 4],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark4.jpg', 4],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark5.jpg', 4],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/shark6.jpg', 4]]
+      return db.queryAsync(`
+      INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+        [photos])
+    })
+
+    .then(() => {
+      var photos = [
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon1.jpg', 5],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon2.jpg', 5],
+        ['https://petsy-caro3usel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon3.jpg', 5],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon4.jpg', 5],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon5.jpg', 5],
+        ['https://petsy-carousel-images.s3.us-east-2.amazonaws.com/Petsy+images/watermelon6.jpg', 5]]
+      return db.queryAsync(`
+      INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+        [photos])
+    })
+
+    // seed 145 more sets of photos using faker
+    .then(() => {
+      for (var i = 6; i < 150; i++) {
+        for (var j = 0; j < 5; j++) {
+          var randomPhoto = faker.image.cats();
+          seededPhotos.push([randomPhoto, i])
+        }
+      }
+    })
+    .then(() => db.queryAsync(`
+    INSERT IGNORE INTO Photos (url, productId) VALUES ?`,
+      [seededPhotos]))
+    .then(() => console.log('finished seeding Photos table'))
 
     // When setup and seeding is complete, alert the user
     .then(() => console.log('Database setup and seeding complete!'))
