582d5a4e26d8e2b79a06100bd3d57116664a1897
Stuzerain <shanford3@gmail.com>
2020-09-28 07:36:34 -0700

    Implement rendering of images and API request on component load

---
+++ client/src/components/ThumbnailStack.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Stack = ({ photos }) => {
+
+};
+
+export default Stack;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import Stack from './components/ThumbnailStack.jsx';
 
 const App = () => {
 
-  // initialize
-  const [photos, setPhotos] = useState(['testing', 'number 2', 'hooks are cool']);
+  // initialize state
+  const [photos, setPhotos] = useState([]);
   const [main, setMain] = useState('');
 
-  const carousel = photos.map((photo) =>
-    <div onClick={() => setMain(photo)}>{photo}</div>
+  // utilize useEffect hook to send GET to server with given ID
+  useEffect(() => {
+    axios.get('http://localhost:3001/products/1')
+      .then((response) => {
+        console.log(response);
+        setPhotos(response.data)
+      })
+  }, [])
+
+  // render one div for each 'image' in state.photos
+  const carousel = photos.map((photo, index) =>
+    <img src={photo.url}
+      width='50'
+      height='50'
+      onClick={() => setMain(photo.url)}
+      key={index} />
   );
 
   return (
     <div>
       This is working! <br />
       {carousel} <br />
-      <div>{main}</div>
-
+      <img src={main} />
     </div>
   );
 
