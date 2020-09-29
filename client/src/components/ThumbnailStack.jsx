87d85df8135ad0938acf767503bdfa94b731ae88
Stuzerain <shanford3@gmail.com>
2020-09-29 08:16:30 -0700

    Added border around thumbnail of main image on stack

--- client/src/components/ThumbnailStack.jsx
+++ client/src/components/ThumbnailStack.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Stack = ({ photos, chooseMain }) => {
+const Stack = ({ photos, chooseMain, mainID }) => {
+  // the mainID prop is used to highlight the thumbnail matching the main
+  // photo with a black border, as Etsy does. If the mainID matches that particular
+  // photo's id, it gets the class 'active', otherwise nothing happens
 
   // render one div for each 'image' in photos
   // on click of any thumbnail, set state of 'main' to be that photo
   const carousel = photos.map((photo, index) =>
     <img
-      className='thumbnail'
+      className={`thumbnail ${mainID === photo.id ? 'active' : ''}`}
       src={photo.url}
-      onClick={() => chooseMain(photo)}
+      onClick={() => { chooseMain(photo); }}
       key={index}
     />
   );
