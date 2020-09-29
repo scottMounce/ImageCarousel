3f10873cfa1288d3cc64fb5441343fb7c178c9c4
Stuzerain <shanford3@gmail.com>
2020-09-29 09:26:30 -0700

    Separate server setup and routing, simplified tests using supertest

--- server/index.js
+++ server/index.js
@@ -1,54 +1,5 @@
-const express = require('express');
-const db = require('../database/db.js');
-
-const app = express();
+const app = require('./app.js');
 const PORT = 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// variable to ensure we only connect to the database on the first request
-let connectedToDB = false;
-
-// serve static html file on root request
-app.use(express.static(__dirname + '/../public'));
-
-// respond to a request with an id param with array of
-// all photos matching that product id
-app.get('/products/:id', (req, res) => {
-  // this variable simplifies our db queries -- it asks for an array of objects of
-  // photo data that has the photo ID, its url, and the name of the product
-  var photoReq = `
-  SELECT
-  Photos.id, product, url
-  FROM Products
-  INNER JOIN Photos
-  ON Products.id = Photos.productID
-  WHERE Products.id = ${req.params.id}`;
-
-  // we check for the var connectedToDV because we only want to connect once,
-  // if we try to connect multiple times we get an error because we try to connect
-  // while we are already connected -- the first call connects to our imageCarousel
-  // database and sets connectedToDB to true so we can simply query from that point on
-  if (!connectedToDB) {
-    db.connectAsync()
-      .then(() => db.queryAsync(`USE imageCarousel`))
-      .then(() => db.queryAsync(photoReq))
-      // use response[0] because MySQL also sends back additional information
-      // we don't want -- response[0] is an array of all photos that match the
-      // request productId
-      .then(dbResults => {
-        connectedToDB = true;
-        res.json(dbResults[0]);
-      });
-  } else {
-    db.queryAsync(photoReq)
-      // see note about request above for why dbResults[0] is sent
-      .then(dbResults => {
-        res.json(dbResults[0]);
-      });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
--- server/server.test.js
+++ server/server.test.js
@@ -1,82 +1,56 @@
 const jest = require('jest');
-const app = require('./index.js');
-const axios = require('axios');
-const toBeType = require('jest-tobetype');
-expect.extend(toBeType);
+const app = require('./app.js');
 const request = require('supertest');
 
 describe('server', () => { // start of server tests
 
   test('request to root responds with 200 (success)', () => {
-    return axios.get('http://localhost:3001/')
-      .then(response => {
-        return expect(response.status).toBe(200);
-      })
-      .catch(err => {
-        throw err;
-      });
+    return request(app)
+      .get('/')
+      .expect(200);
   });
 
   test('request to /products/:id is successful', () => {
-    return axios.get('http://localhost:3001/products/1')
-      .then(response => {
-        return expect(response.status).toBe(200);
-      })
-      .catch(err => {
-        throw err;
-      });
+    return request(app)
+      .get('/products/1')
+      .expect(200)
   });
 
   test('request to /products/:id responds with array', () => {
-    return axios.get('http://localhost:3001/products/1')
-      .then(response => {
-        return expect(response.data).toBeType('array');
-      })
-      .catch(err => {
-        throw err;
-      });
+    return request(app)
+      .get('/products/1')
+      .expect(200)
+      .then(response => Array.isArray(response.body))
   });
 
-  test('array response full of objects', () => {
-    return axios.get('http://localhost:3001/products/1')
-      .then(response => {
-        return expect(response.data[0]).toBeType('object');
-      })
-      .catch(err => {
-        throw err;
-      });
+  test('array response items are objects', () => {
+    return request(app)
+      .get('/products/1')
+      .expect(200)
+      .then(response => (typeof response.body[0] === 'object' && response.body[0] !== null))
   });
 
   describe('photo objects have id, product, url properties', () => { // photo object tests
 
     test('test photo object id', () => {
-      return axios.get('http://localhost:3001/products/1')
-        .then(response => {
-          return expect(response.data[0].id).toBeTruthy();
-        })
-        .catch(err => {
-          throw err;
-        });
+      return request(app)
+        .get('/products/1')
+        .expect(200)
+        .then(response => expect(response.body[0].id).toBeTruthy())
     });
 
     test('test photo object product', () => {
-      return axios.get('http://localhost:3001/products/1')
-        .then(response => {
-          return expect(response.data[0].product).toBeTruthy();
-        })
-        .catch(err => {
-          throw err;
-        });
+      return request(app)
+        .get('/products/1')
+        .expect(200)
+        .then(response => expect(response.body[0].product).toBeTruthy())
     });
 
     test('test photo object url', () => {
-      return axios.get('http://localhost:3001/products/1')
-        .then(response => {
-          return expect(response.data[0].url).toBeTruthy();
-        })
-        .catch(err => {
-          throw err;
-        });
+      return request(app)
+        .get('/products/1')
+        .expect(200)
+        .then(response => expect(response.body[0].url).toBeTruthy())
     });
 
   }); // end of photo object tests
