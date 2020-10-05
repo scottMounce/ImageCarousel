80f7c1eb642ce0d92f0ad061aa9da67a31d9baac
Stuzerain <shanford3@gmail.com>
2020-10-05 15:18:56 -0700

    Add CORS middleware to server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const db = require('../database/db.js');
+const cors = require('cors');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
